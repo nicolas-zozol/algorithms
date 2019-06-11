a648678395d326cc370ffb01e16918004f3aaddc
Nicolas Zozol <nzozol@gmail.com>
2019-06-11 12:54:00 +0200

    MaxHeap and MinHeap

---
+++ src/binary-heap/min-heap-class.js
@@ -0,0 +1,136 @@
+class MinHeap {
+
+
+    constructor() {
+        this.heap = [0]
+    }
+
+    swap(i, j) {
+        const t = this.heap[i];
+        this.heap[i] = this.heap[j];
+        this.heap[j] = t;
+    }
+
+    size() {
+        return this.heap[0]
+    }
+
+    increase() {
+        this.heap[0] = this.heap[0] + 1;
+    }
+
+    decrease() {
+        this.heap[0] = this.heap[0] - 1;
+        this.heap.pop();
+    }
+
+    leftChildIndex(i) {
+        return 2 * i;
+    }
+
+    rightChildIndex(i) {
+        return 2 * i + 1;
+    }
+
+
+    parentIndex(i) {
+        return Math.floor(i / 2)
+    }
+
+
+    insert(value) {
+
+        let i = this.size() + 1;
+        this.heap[i] = value;
+        this.increase();
+
+        this.swapInsertion(value, i);
+
+    }
+
+
+    swapInsertion(value, i) {
+        if (i === 1) {
+            return; //reached root
+        }
+        let pIndex = this.parentIndex(i);
+        let parentValue = this.heap[pIndex];
+
+
+            // we want smaller up, so swicth parentValue and Value
+            if (this.firstIsBigger(parentValue, value)) {
+                this.swap(pIndex, i);
+                this.swapInsertion(value, pIndex);
+            }
+
+    }
+
+
+    remove(i) {
+        this.swap(i, this.size());
+        this.decrease();
+        this.subify(i);
+
+
+    }
+
+    heapify(tree, limit = size(tree)) {
+
+
+        let pIndex = this.parentIndex(limit);
+        for (let i = pIndex; i >= 1; i--) {
+            this.subify(i, limit);
+        }
+
+    }
+
+    subify(i, limit = this.size()) {
+        let leftIndex = this.leftChildIndex(i);
+
+        if (leftIndex > limit) {
+            return;
+        }
+
+        let rightIndex = this.rightChildIndex(i);
+        let leftValue = this.heap[leftIndex];
+        let rightValue = this.heap[rightIndex];
+
+
+        let smallerChildrenIndex = leftIndex;
+
+        if (rightIndex <= limit && this.firstIsSmaller(rightValue , leftValue)) {
+            smallerChildrenIndex = rightIndex;
+        }
+
+        if (this.firstIsSmaller(this.heap[smallerChildrenIndex] , this.heap[i])) {
+            this.swap(i, smallerChildrenIndex);
+            this.subify(smallerChildrenIndex, limit)
+        }
+
+    }
+
+    firstIsSmaller(x, y) {
+
+        return x < y;
+
+    }
+
+    firstIsBigger(x, y) {
+
+        return x > y;
+
+    }
+}
+
+
+let heap = new MinHeap();
+heap.insert(12);
+heap.insert(10);
+heap.insert(1);
+heap.insert(2);
+heap.insert(2);
+heap.insert(6);
+heap.insert(6);
+heap.remove(1);
+
+console.log(heap.heap);
